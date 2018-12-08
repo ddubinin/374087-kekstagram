0337351569612dcf0aeab23ac15ecc51a1d0760c
Dmitry <dubinin.dmitry@mail.ru>
2018-12-08 22:01:33 +0300

    еще разок

--- js/pictures.js
+++ js/pictures.js
@@ -141,12 +141,13 @@ document.addEventListener('keydown', function (evt) {
   }
 });
 
+
 // загрзузка своей картинки
 var uploadInput = document.querySelector('#upload-file');
 var uploadForm = document.querySelector('.img-upload__overlay');
 
 var uploadFormEscClose = function (evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
+  if (evt.keyCode === ESC_KEYCODE && focusOrBlur !== 'focus') {
     uploadInput.value = '';
     uploadFormClose();
   }
@@ -194,3 +195,76 @@ var getEffectPicture = function (effect) {
     effectBar.classList.add('hidden');
   }
 };
+// хэштег
+var hashtagInput = document.querySelector('.text__hashtags');
+
+var focusOrBlur = '';
+
+hashtagInput.addEventListener('focus', function () {
+  focusOrBlur = 'focus';
+
+});
+hashtagInput.addEventListener('blur', function () {
+  focusOrBlur = 'blur';
+});
+
+hashtagInput.addEventListener('change', function (e) {
+  var val = hashtagInput.value.trim();
+  var tags = val.split(' ').filter(function (tag) {
+    return tag !== '';
+  });
+  var validationResult = hashTagsValidate(tags);
+  e.target.setCustomValidity(validationResult);
+});
+
+
+// валидация всей строки
+var hashTagsValidate = function (tags) {
+  if (tags.length > 5) {
+    return 'Не должно превышать 5';
+  }
+  if (checkTagRepit(tags)) {
+    return 'не должны повторяться';
+  }
+
+  var result = tags.map(function (tag) {
+    return tagValidate(tag);
+  })
+    .filter(function (err) {
+      return err !== '';
+    });
+
+  return result.length === 0 ? '' : result[0];
+};
+
+// валидация отдельного тега
+var tagValidate = function (tag) {
+  tag = tag.toUpperCase();
+  var fl = tag.slice(0, 1);
+  if (fl !== '#') {
+    return 'должен начинаться с #';
+  }
+  if (tag.length > 20) {
+    return 'не должно быть символов больше 20';
+  }
+
+  tag = tag.slice(1);
+  if (/#/.test(tag)) {
+    return 'должны разделяться пробелами';
+  }
+  return '';
+};
+
+var checkTagRepit = function (tags) {
+  var tagsMap = {};
+  var validationError = false;
+  tags.forEach(function (tag) {
+    if (tagsMap.hasOwnProperty(tag)) {
+      validationError = true;
+    } else {
+      tagsMap[tag] = true;
+    }
+  });
+  return validationError;
+};
+
